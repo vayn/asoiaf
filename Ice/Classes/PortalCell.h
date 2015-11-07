57ee31a69e35727227299c204bd958452176e1fe
Vayn a.k.a. VT <vayn@vayn.de>
2015-11-07 21:40:51 +0800

    add activity indicator in portal cell

--- Ice/Classes/PortalCell.h
+++ Ice/Classes/PortalCell.h
@@ -12,5 +12,6 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *portalImageView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loadingIndicator;
 
 @end
