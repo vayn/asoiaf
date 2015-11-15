6ae47c2ea8dd4b1b6ddb23ca63e70f7ca21d28b9
Vayn a.k.a. VT <vayn@vayn.de>
2015-11-15 23:41:35 +0800

    updated parallax header view
    From Nir Zhihu 2.0

--- A Song of Ice and Fire/Library/ParallaxHeaderView/ParallaxHeaderView.h
+++ A Song of Ice and Fire/Library/ParallaxHeaderView/ParallaxHeaderView.h
@@ -9,14 +9,24 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol ParallaxHeaderViewDelegate <NSObject>
+
+- (void)lockDirection;
+
+@end
+
 @interface ParallaxHeaderView : UIView
 
-@property (nonatomic) IBOutlet UILabel *headerTitleLabel;
-@property (nonatomic) UIImage *headerImage;
+@property (nonatomic, weak) UILabel *headerTitleLabel;
+@property (nonatomic, weak) id<ParallaxHeaderViewDelegate> delegate;
+@property (nonatomic, weak) UIImage *blurViewImage;
 
-+ (id)parallaxHeaderViewWithImage:(UIImage *)image forSize:(CGSize)headerSize;
-+ (id)parallaxHeaderViewWithSubView:(UIView *)subView;
++ (id)parallaxHeaderViewWithSubView:(UIView *)subView forSize:(CGSize)headerSize;
++ (id)parallaxWebHeaderViewWithSubView:(UIView *)subView forSize:(CGSize)headerSize;
++ (id)parallaxThemeHeaderViewWithSubView:(UIView *)subView forSize:(CGSize)headerSize andImage: (UIImage *)blurViewImageParam;
 - (void)layoutHeaderViewForScrollViewOffset:(CGPoint)offset;
+- (void)layoutWebHeaderViewForScrollViewOffset:(CGPoint)offset;
+- (void)layoutThemeHeaderViewForScrollViewOffset:(CGPoint)offset;
 - (void)refreshBlurViewForNewImage;
 
 @end
