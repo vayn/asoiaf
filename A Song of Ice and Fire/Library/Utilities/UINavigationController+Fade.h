10712495d2bbde35164469a27dd72122787c4fb4
Vayn a.k.a. VT <vayn@vayn.de>
2015-12-05 10:04:59 +0800

    custom navigation controller push and pop animation

---
+++ A Song of Ice and Fire/Library/Utilities/UINavigationController+Fade.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Fade.h
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/5.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Fade)
+
+@end
---
+++ A Song of Ice and Fire/Library/Utilities/UINavigationController+Fade.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Fade.m
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/5.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import "UINavigationController+Fade.h"
+
+@implementation UINavigationController (Fade)
+
+@end
