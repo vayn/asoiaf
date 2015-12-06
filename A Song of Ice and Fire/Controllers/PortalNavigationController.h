88b77a7267fcdafeb8d9a8a65f8403114f0077a5
Vayn a.k.a. VT <vayn@vayn.de>
2015-12-07 04:52:19 +0800

    add custom navigation controller (not finished yet)

---
+++ A Song of Ice and Fire/Controllers/PortalNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  PortalNavigationController.h
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/7.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PortalNavigationController : UINavigationController
+
+@end
