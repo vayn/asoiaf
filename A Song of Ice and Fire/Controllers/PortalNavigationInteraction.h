e04f661d7629a0a821e9a672ff12e167e0514565
Vayn a.k.a. VT <vayn@vayn.de>
2015-12-10 12:12:56 +0800

    add custom interaction of navigation transition

---
+++ A Song of Ice and Fire/Controllers/PortalNavigationInteraction.h
@@ -0,0 +1,13 @@
+//
+//  PortalNavigationInteraction.h
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/9.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PortalNavigationInteraction : NSObject
+
+@end
---
+++ A Song of Ice and Fire/Controllers/PortalNavigationInteraction.m
@@ -0,0 +1,13 @@
+//
+//  PortalNavigationInteraction.m
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/9.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import "PortalNavigationInteraction.h"
+
+@implementation PortalNavigationInteraction
+
+@end
