af07a430793e5294aa26403c6119f9f332e7b9a3
Vicent Tsai <vayn@vayn.de>
2015-12-12 07:05:23 +0800

    add WKWebview category

---
+++ A Song of Ice and Fire/Library/Utilities/Category/WKWebView+SynchronousEvaluateJavaScript.h
@@ -0,0 +1,13 @@
+//
+//  WKWebView+SynchronousEvaluateJavaScript.h
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/12.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import <WebKit/WebKit.h>
+
+@interface WKWebView (SynchronousEvaluateJavaScript)
+
+@end
---
+++ A Song of Ice and Fire/Library/Utilities/Category/WKWebView+SynchronousEvaluateJavaScript.m
@@ -0,0 +1,13 @@
+//
+//  WKWebView+SynchronousEvaluateJavaScript.m
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/12.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import "WKWebView+SynchronousEvaluateJavaScript.h"
+
+@implementation WKWebView (SynchronousEvaluateJavaScript)
+
+@end
