77b2ade25edc831e7f20b5e90238354627527a10
Vicent Tsai <vayn@vayn.de>
2015-12-12 07:09:00 +0800

    add method

--- A Song of Ice and Fire/Library/Utilities/Category/WKWebView+SynchronousEvaluateJavaScript.m
+++ A Song of Ice and Fire/Library/Utilities/Category/WKWebView+SynchronousEvaluateJavaScript.m
@@ -10,4 +10,28 @@
 
 @implementation WKWebView (SynchronousEvaluateJavaScript)
 
+- (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)script
+{
+    __block NSString *resultString = nil;
+    __block BOOL finished = NO;
+
+    [self evaluateJavaScript:script completionHandler:^(id result, NSError *error) {
+        if (error == nil) {
+            if (result != nil) {
+                resultString = [NSString stringWithFormat:@"%@", result];
+            }
+        } else {
+            NSLog(@"evaluateJavaScript error : %@", error.localizedDescription);
+        }
+        finished = YES;
+    }];
+
+    while (!finished)
+    {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    }
+
+    return resultString;
+}
+
 @end
