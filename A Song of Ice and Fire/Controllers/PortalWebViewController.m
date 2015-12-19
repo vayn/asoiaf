229d5dd86a1c7c0dd0317a156a142dbe2031d1da
Vicent Tsai <vayn@vayn.de>
2015-12-20 03:49:04 +0800

    设置 status bar

--- A Song of Ice and Fire/Controllers/PortalWebViewController.m
+++ A Song of Ice and Fire/Controllers/PortalWebViewController.m
@@ -14,9 +14,11 @@
 #import "Spinner.h"
 #import "UINavigationController+TransparentNavigationBar.h"
 
-@interface PortalWebViewController ()
+@interface PortalWebViewController () <UIScrollViewDelegate>
 
 @property (nonatomic, strong) Spinner *cubeSpinner;
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIView *statusBackgroundView;
 
 @end
 
@@ -35,23 +37,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.cubeSpinner.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-    [self.view addSubview:self.cubeSpinner];
-    [self.cubeSpinner startAnimating];
-
     [self setupWebView];
+    [self setupStatusBar];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+
     [self.navigationController setTransparentNavigationBar];
+    self.navigationController.navigationBar.barStyle = UIStatusBarStyleLightContent;
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
+
     [self.navigationController restoreDefaultNavigationBar];
+    self.navigationController.navigationBar.barStyle = UIStatusBarStyleDefault;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -61,6 +64,10 @@
 
 - (void)setupWebView
 {
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame];
+    self.webView.scrollView.delegate = self;
+    self.webView.scrollView.bounces = NO;
+
     // Main bundle
     NSBundle *mainBundle = [NSBundle mainBundle];
 
@@ -80,8 +87,11 @@
                                        options:NSLiteralSearch
                                          range:NSMakeRange(0, portalTemplate.length)];
 
-    WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.frame];
     NSString *link = [self.category.link stringByReplacingOccurrencesOfString:@"Category" withString:@"Portal"];
+
+    self.cubeSpinner.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    [self.view addSubview:self.cubeSpinner];
+    [self.cubeSpinner startAnimating];
 
     [[MainManager sharedManager]getWikiEntry:link completionBlock:^(NSString *wikiEntry) {
         [portalTemplate replaceOccurrencesOfString:@"[[[content]]]"
@@ -93,9 +103,38 @@
         [self.cubeSpinner setHidden:YES];
         [self.cubeSpinner removeFromSuperview];
 
-        [webView loadHTMLString:portalTemplate baseURL:[NSURL fileURLWithPath:cssPath]];
-        [self.view addSubview:webView];
+        [self.webView loadHTMLString:portalTemplate baseURL:[NSURL fileURLWithPath:cssPath]];
+        [self.view addSubview:self.webView];
     }];
+}
+
+- (void)setupStatusBar
+{
+    CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
+    CGRect frame = CGRectMake(0, 0, statusBarSize.width, statusBarSize.height);
+    self.statusBackgroundView = [[UIView alloc] initWithFrame:frame];
+    self.statusBackgroundView.backgroundColor = [UIColor whiteColor];
+    self.statusBackgroundView.alpha = 0.0;
+}
+
+#pragma mark - UIScrollView Delegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGPoint offset = scrollView.contentOffset;
+    UIStatusBarStyle barStyle = (UIStatusBarStyle)self.navigationController.navigationBar.barStyle;
+
+    if (offset.y >= 270 && barStyle == UIStatusBarStyleLightContent) {
+        [self.view addSubview:self.statusBackgroundView];
+        [UIView animateWithDuration:0.25 animations:^{
+            self.navigationController.navigationBar.barStyle = UIStatusBarStyleDefault;
+            self.statusBackgroundView.alpha = 1.0;
+        }];
+    } else if (offset.y < 270 && barStyle == UIStatusBarStyleDefault) {
+        self.navigationController.navigationBar.barStyle = UIStatusBarStyleLightContent;
+        self.statusBackgroundView.alpha = 0.0;
+        [self.statusBackgroundView removeFromSuperview];
+    }
 }
 
 @end
