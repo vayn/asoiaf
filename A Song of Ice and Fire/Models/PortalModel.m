9140ff5a633a98a310fd306694e2f54cd5b9c23d
Vicent Tsai <vayn@vayn.de>
2015-12-21 00:51:15 +0800

    增加 PortalModel

---
+++ A Song of Ice and Fire/Models/PortalModel.m
@@ -0,0 +1,13 @@
+//
+//  PortalModel.m
+//  A Song of Ice and Fire
+//
+//  Created by Vicent Tsai on 15/12/20.
+//  Copyright © 2015年 HeZhi Corp. All rights reserved.
+//
+
+#import "PortalModel.h"
+
+@implementation PortalModel
+
+@end
