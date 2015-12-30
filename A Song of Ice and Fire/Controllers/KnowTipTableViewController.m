86567fbee7111c0bc1935f76ab863a0990f752ce
Vicent Tsai <vayn@vayn.de>
2015-12-30 09:47:46 +0800

    调整 know tip title 位置

--- A Song of Ice and Fire/Controllers/KnowTipTableViewController.m
+++ A Song of Ice and Fire/Controllers/KnowTipTableViewController.m
@@ -117,7 +117,7 @@
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, tableView.frame.size.width, 18)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 18)];
     label.font = [UIFont systemFontOfSize:17];
     label.text = [self tableView:tableView titleForHeaderInSection:section];
 
